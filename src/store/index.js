c9ea747493c134f6e53415343c8fa1862c0e85bc
JeanCRuiz <139879016+JeanCRuiz@users.noreply.github.com>
2023-07-31 08:35:59 -0400

    testing

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import { defineStore } from 'pinia';
-import data from '../data.json';
-import { currentId, dateFormatted } from '../helper';
+import { defineStore } from "pinia";
+import data from "../data.json";
+import { currentId, dateFormatted } from "../helper";
 
-export const useCommentStore = defineStore('commentStore', {
+export const useCommentStore = defineStore("commentStore", {
   state: () => {
     return {
       currentUser: data.currentUser,
@@ -28,30 +28,30 @@ export const useCommentStore = defineStore('commentStore', {
         replies: [],
       };
       await this.comments.push(newComment);
-      comment = '';
+      comment = "";
       this.scrollToLastComment();
     },
     // When a comment is added, scroll to the last comment added
     scrollToLastComment() {
-      const lastComment = document.querySelector('#comments').lastElementChild;
-      lastComment.scrollIntoView({ behavior: 'smooth' });
+      const lastComment = document.querySelector("#comments").lastElementChild;
+      lastComment.scrollIntoView({ behavior: "smooth" });
     },
-    // async addReply(comment, reply) {
-    //   const newReply = {
-    //     id: currentId(data),
-    //     createdAt: dateFormatted(new Date()),
-    //     user: this.currentUser,
-    //     score: 0,
-    //     content: reply,
-    //   };
-    //   await comment.replies.push(newReply);
-    //   reply = '';
-    //   this.scrollToLastReply();
-    // },
-    // // When a reply is added, scroll to the last reply added
-    // scrollToLastReply() {
-    //   const lastReply = document.querySelector('#replies').lastElementChild;
-    //   lastReply.scrollIntoView({ behavior: 'smooth' });
-    // },
+    async addReply(comment, reply) {
+      const newReply = {
+        id: currentId(data),
+        createdAt: dateFormatted(new Date()),
+        user: this.currentUser,
+        score: 0,
+        content: reply,
+      };
+      await comment.replies.push(newReply);
+      reply = "";
+      this.scrollToLastReply();
+    },
+    // When a reply is added, scroll to the last reply added
+    scrollToLastReply() {
+      const lastReply = document.querySelector("#replies").lastElementChild;
+      lastReply.scrollIntoView({ behavior: "smooth" });
+    },
   },
 });
